a0941b9de12986812db88d17cc1a37608add4a84
Saurabh <saurabhsharma090903@gmail.com>
2026-05-18 00:09:06 +0530

    we Created the calculator for the backend that runs on the terminal (We will optimise the code in future)

--- Backend_Calculator/backend_calc.js
+++ Backend_Calculator/backend_calc.js
@@ -1,4 +1,4 @@
-console.log("This is our Calculator:");
+console.log("This is our Calculator");
 
 process.stdout.write("Enter the First Number: ");
 
@@ -10,6 +10,45 @@ function firstNumberReader() {
   });
 }
 
-const num1 = firstNumberReader();
+let num1 = await firstNumberReader();
+num1 = Number(num1);
 
-process.stdout.write("\n Enter the Second Number: ");
+process.stdout.write("Enter the Second Number: ");
+
+function secondNumberReader() {
+  return new Promise((resolve) => {
+    process.stdin.once("data", (chunks) => {
+      resolve(chunks.toString().trim());
+    });
+  });
+}
+
+let num2 = await secondNumberReader();
+num2 = Number(num2);
+
+process.stdout.write("\n Enter the operator (+,-,*,/) : ");
+
+function operatorReader() {
+  return new Promise((resolve) => {
+    process.stdin.once("data", (chunks) => {
+      resolve(chunks.toString().trim());
+    });
+  });
+}
+
+const operator = await operatorReader();
+
+switch (operator) {
+  case "+":
+    console.log("The summation of the Two numbers are ", num1 + num2);
+    break;
+  case "-":
+    console.log("The difference of the Two numbers are ", num1 - num2);
+    break;
+  case "*":
+    console.log("The product of the Two numbers are ", num1 * num2);
+    break;
+  case "/":
+    console.log("The ratio of the Two number are ", num1 / num2);
+    break;
+}
